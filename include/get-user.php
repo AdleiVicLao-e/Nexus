aeff050b13f60250f1162516d33f14d5e468611b
ronan000 <2200465@slu.edu.ph>
2024-09-26 12:35:21 +0800

    User Table in Admin

--- include/get-user.php
+++ include/get-user.php
@@ -13,21 +13,21 @@ if ($conn->connect_error) {
 }
 
 // SQL query to fetch data from user_log table
-$sql = "SELECT id, user_name, user_school, time FROM user_log";
+$sql = "SELECT user_number, user_name, user_school, time FROM user_log";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // Output data of each row
     echo "<table border='1'>
             <tr>
-                <th>ID</th>
+                <th>User Number</th>  <!-- Change from ID to User Number -->
                 <th>User Name</th>
                 <th>User School</th>
                 <th>Time</th>
             </tr>";
     while($row = $result->fetch_assoc()) {
         echo "<tr>
-                <td>" . $row["id"] . "</td>
+                <td>" . $row["user_number"] . "</td>  <!-- Change from id to user_number -->
                 <td>" . $row["user_name"] . "</td>
                 <td>" . $row["user_school"] . "</td>
                 <td>" . $row["time"] . "</td>
