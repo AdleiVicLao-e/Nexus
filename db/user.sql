088ba5d6c3e0ecd365e8b0d41c29029b739b4ee8
Valiant Mi-ing <2212579@slu.edu.ph>
2024-11-11 22:13:50 +0800

    Updated user db

--- db/user.sql
+++ db/user.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 21, 2024 at 02:01 PM
--- Server version: 5.7.40
--- PHP Version: 8.0.26
+-- Generation Time: Nov 11, 2024 at 02:13 PM
+-- Server version: 8.3.0
+-- PHP Version: 8.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,17 +28,23 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `feedback`;
-
 CREATE TABLE IF NOT EXISTS `feedback` (
-  `feedbackId` int(11) NOT NULL AUTO_INCREMENT,
-  `date` DATE NOT NULL,
-  `quality_presentation` VARCHAR(255) NOT NULL,
-  `cleanliness_ambiance` VARCHAR(255) NOT NULL,
-  `staff_service` VARCHAR(255) NOT NULL,
-  `overall_experience` VARCHAR(255) NOT NULL,
-  `comments` TEXT,
+  `feedbackId` int NOT NULL,
+  `date` date NOT NULL,
+  `quality_presentation` varchar(255) NOT NULL,
+  `cleanliness_ambiance` varchar(255) NOT NULL,
+  `staff_service` varchar(255) NOT NULL,
+  `overall_experience` varchar(255) NOT NULL,
+  `comments` text,
   PRIMARY KEY (`feedbackId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`feedbackId`, `date`, `quality_presentation`, `cleanliness_ambiance`, `staff_service`, `overall_experience`, `comments`) VALUES
+(1, '2024-11-11', 'Dissatisfied', 'Dissatisfied', 'Dissatisfied', 'Dissatisfied', 'Good ');
 
 -- --------------------------------------------------------
 
@@ -48,12 +54,19 @@ CREATE TABLE IF NOT EXISTS `feedback` (
 
 DROP TABLE IF EXISTS `user_log`;
 CREATE TABLE IF NOT EXISTS `user_log` (
-  `user_number` int(11) NOT NULL,
-  `user_name` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `user_school` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `user_number` int NOT NULL,
+  `user_name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `user_school` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `time` datetime NOT NULL,
   PRIMARY KEY (`user_number`)
 ) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=armscii8;
+
+--
+-- Dumping data for table `user_log`
+--
+
+INSERT INTO `user_log` (`user_number`, `user_name`, `user_school`, `time`) VALUES
+(1, 'Valiant', 'School of Law', '2024-11-11 22:12:22');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
